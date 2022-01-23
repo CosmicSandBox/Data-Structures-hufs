802aeb5102be7e37c2b03c445615e39d44b590d6
CosmicSandBox <kimjaewoo13579@gmail.com>
2022-01-23 23:59:46 +0900

    gcd print

--- gcd.py
+++ gcd.py
@@ -1,8 +1,9 @@
-from re import A
-
-
 #최대 공약수
-while a!=0 and b!=0:
-    if a>b : a=a-b
-    else : b=b-a
-return a+b
+a=int(input('a입력'))
+b=int(input('b입력'))
+while a!=0 and b!=0 :
+    if a>b :
+        a=a-b
+    else :
+        b=b-a
+print('최대공약수%d' %(a+b))
